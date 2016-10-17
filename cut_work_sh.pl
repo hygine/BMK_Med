1a7695aa39d1643866b67f5f20a62c94e196f715
Hygine <haijunlin@163.com>
2016-10-17 12:50:54 +0800

    Add files via upload

---
+++ cut_work_sh.pl
@@ -0,0 +1,176 @@
+#!/usr/bin/perl
+use strict;
+
+
+open IN, "/lustre/nas1/liaoqp/project/BMK160531-C097-01-a.3/Analysis/Basic_Analysis/Tophat_Cufflinks/work_sh/Cuffquant.sh.div.1.sh.12393.qsub/../Cuffquant.sh.div.3.sh" || die;
+my $num = 0;
+
+while (<IN>) {
+	chomp;
+	next if (/^$/);
+	$num++;
+	
+	if($num == 001){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==005){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==012){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==016){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==020){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==024){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==028){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==035){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==043){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==050){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==057){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==067){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==071){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==078){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==086){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==093){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==100){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==110){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==114){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==121){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==129){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==136){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==143){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==153){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==157){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==164){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==172){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==179){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==186){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==190){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==197){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==201){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==208){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==216){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==223){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==230){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==240){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==244){
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}elsif($num ==251)
+	{
+		open OUT, ">/share/nas1/linhj/testing/Cuffquant.sh.div.3_00$num.sh" || die;
+		print OUT $_;
+		close OUT;
+	}else{
+		print "$num is bad !";
+	}
+}
+
+
+
